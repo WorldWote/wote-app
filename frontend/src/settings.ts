cb9101fb6a7ddb558cb4f9821a8e614356080195
Vano Atabegashvili <vano.atabegashvili@gmail.com>
2023-11-18 19:42:58 +0300

    frontend: change network to optimism goerli

--- frontend/src/settings.ts
+++ frontend/src/settings.ts
@@ -1,5 +1,5 @@
-import {goerli} from "viem/chains";
+import { optimismGoerli } from 'viem/chains';
 
-export const DEFAULT_CHAIN = goerli;
+export const DEFAULT_CHAIN = optimismGoerli;
 export const PROJECT_ID = '45b75ad086285fcbfb457b8ab0a94f1c';
 export const WORLDCOIN_APP_ID = 'app_staging_8e51b49daa766cfd178b3c6495f0d61a';
